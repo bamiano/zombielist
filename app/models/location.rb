da2f67d21bce293e9ba87073054450969da17fa2
ybinstock <ybinstock@gmail.com>
2014-11-21 14:25:22 -0800

    Yoni's second changes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,6 @@
 class Location < ActiveRecord::Base
 	belongs_to :user
 
-	validates :location_type, :name, :address,
-    presence: true
-  
+	validates :location_type, :name, :address, presence: true
+
 end
